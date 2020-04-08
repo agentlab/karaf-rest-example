79afcd316cc0919f59387052b756c8e3c90fc7c0
DmitryZagr <bikemaster2000@list.ru>
2020-04-08 23:24:07 +0300

    added cors

--- ru.agentlab.cors/src/main/java/ru/agentlab/oauth/impl/CorsFilter.java
+++ ru.agentlab.cors/src/main/java/ru/agentlab/oauth/impl/CorsFilter.java
@@ -14,13 +14,16 @@ import org.osgi.service.component.annotations.Component;
 @Component(property = { "osgi.jaxrs.extension=true" })
 @Provider
 public class CorsFilter implements ContainerResponseFilter {
+
+    private static final String ALLOW_ORIGIN = getEnv("ALLOW_ORIGIN", "http://example.com:3000");
+
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
             throws IOException {
 
         MultivaluedMap<String, Object> headers = responseContext.getHeaders();
         headers.add("Access-Control-Allow-Credentials", true);
-        headers.add("Access-Control-Allow-Origin", "*");
+        headers.add("Access-Control-Allow-Origin", ALLOW_ORIGIN);
         headers.addAll("Access-Control-Allow-Methods", HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT,
                 HttpMethod.DELETE, HttpMethod.OPTIONS, HttpMethod.HEAD);
         headers.addAll("Access-Control-Allow-Headers",
@@ -28,5 +31,10 @@ public class CorsFilter implements ContainerResponseFilter {
         headers.addAll("Access-Control-Expose-Headers",
                 "Set-Cookie, Cache-Control, Content-Language, Content-Length, Content-Type, Expires, Last-Modified, Pragma");
         headers.add("Access-Control-Max-Age", 86400);
+    }
+
+    private static String getEnv(String key, String def) {
+        String value = System.getenv(key);
+        return value != null ? value : def;
     }
 }
